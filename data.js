cd4940199bd253b62e758f2dedb41bd7f88c0b5f
Art <jirabhatpv2003@gmail.com>
2025-08-05 06:05:22 +0700

    art

--- data.js
+++ data.js
@@ -1,32 +1,66 @@
-// data.js
 const products = [
   {
     id: 1,
     name: "ID ROV แรงค์ Conqueror",
     price: 200,
     stock: 5,
-    image: "https://server-avatar.nimostatic.tv/201904111555002197840_1629513688346_avatar.png"
+    image: "https://server-avatar.nimostatic.tv/201904111555002197840_1629513688346_avatar.png",
+    description: "บัญชี ROV แรงค์ Conqueror มีสกินหายากมากมาย ฮีโร่ครบ เหมาะสำหรับสายแข่งขันและผู้เล่นที่ต้องการความโดดเด่นในเกม"
   },
   {
     id: 2,
     name: "ID Free Fire มีตัวละครเต็ม",
     price: 150,
     stock: 3,
-    image: "https://static-cdn.jtvnw.net/jtv_user_pictures/43aa2943-730e-427c-bcec-6cdef4d4c4d1-profile_banner-480.jpeg"
+    image: "https://static-cdn.jtvnw.net/jtv_user_pictures/43aa2943-730e-427c-bcec-6cdef4d4c4d1-profile_banner-480.jpeg",
+    description: "ID Free Fire ตัวละครครบทุกตัว พร้อมสกินปืนและชุดแฟชั่นมากมาย เติมเพชรแล้ว พร้อมเล่นได้ทันที"
   },
   {
     id: 3,
     name: "ID PUBG Mobile มีสกินปืน",
     price: 250,
     stock: 2,
-    image: "img/pubg.jpg"
+    image: "https://www.pubgmobile.com/images/event/global_arms_design_contest/m/track/track_gun1.png",
+    description: "ID PUBG Mobile มีสกินปืนระดับแรร์ ชุดแฟชั่นครบ พร้อมสำหรับสายยิงและสายแฟชั่น"
   },
   {
-  id: 4,
-  name: "ID Genshin AR60 ตัว 5 ดาวครบ",
-  price: 400,
-  stock: 1,
-  image: "img/genshin.jpg"
-}
-
+    id: 4,
+    name: "ID Genshin AR60 ตัว 5 ดาวครบ",
+    price: 400,
+    stock: 1,
+    image: "https://img.lazcdn.com/g/p/94cadc8a78538c1a30eae2fcad1eb8f6.jpg_360x360q75.jpg_.webp",
+    description: "ID Genshin Impact AR60 ตัวละคร 5 ดาวครบทุกตัว อาวุธเทพ เควสหลักและกิจกรรมผ่านหมด เหมาะสำหรับแฟนเกมสายสะสม"
+  },
+  {
+    id: 5,
+    name: "ID ASTDX Unbound บัฟ AllSTAR พร้อมเล่น",
+    price: 100,
+    stock: 9,
+    image: "https://robloxid.com/wp-content/sites/robloxid/2025/07/roblox-all-star-tower-defense-x-codes-550x309.jpg",
+    description: "ID ASTDX Unbound พร้อมบัฟ AllSTAR สำหรับสายฟาร์มและสายแข่งขัน เข้าเล่นได้ทันที ไม่ต้องปั้นเอง ให้เสียเวลา"
+  },
+  {
+  id: 6,
+  name: "ID ไก่ตันBloxFruit ผลถาวร",
+  price: 200,
+  stock: 17,
+  image: "https://pbs.twimg.com/media/Go6rUyhXgAAvWZi?format=jpg&name=large",
+  description: "ID BloxFruit ผลถาวร ไก่ตัน มีผลปีศาจถาวรในตัว พร้อมเงินและเลเวลสูง เหมาะสำหรับผู้เล่นที่ต้องการเริ่มต้นแบบเทพ ไม่ต้องสุ่มผลเอง"
+  },
+  {
+  id: 7,
+  name: "ID ALS บัฟ Glitch ตัวเมต้าพร้อมเล่น",
+  price: 300,
+  stock: 8,
+  image: "https://media.karousell.com/media/photos/products/2025/7/4/anime_last_stand_materials__fa_1751655992_22df835c_thumbnail",
+  description: "ID Anime Last Stand (ALS) มีบัฟ Glitch ตัวเมต้า พร้อมเล่นทันที ตัวละครและไอเทมครบ เหมาะสำหรับสายฟาร์มและสายแข่งขัน"
+  },
+  {
+  id: 8,
+  name: "ID AVanguard บัฟ Monarch ตัวเมต้าพร้อมเล่น",
+  price: 300,
+  stock: 8,
+  image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSbdcCGbv-oppzFVqbpO8C_x4hqLr-2ieCR5g&s",
+  description: "ID AVanguard มีบัฟ Monarch ตัวเมต้า ไอดีเทพพร้อมเล่นทันที มีตัวละครและของหายากครบ เหมาะสำหรับผู้เล่นที่ต้องการความได้เปรียบในเกม"
+  }
 ];
